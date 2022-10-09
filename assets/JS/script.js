9caa814898164feb6edb0459a572265f051ebfa9
Lauren <l.cvengros@icloud.com>
2022-10-09 16:28:50 -0400

    first commit

---
+++ assets/JS/script.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+//     $(this).scrollTop(0);
+// });
+
+$('#recipeCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+  
+
+  //https://www.codeply.com/go/EIOtI7nkP8/bootstrap-carousel-with-multiple-cards
